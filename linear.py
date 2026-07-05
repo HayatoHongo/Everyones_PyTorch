1c43c9471e98e2309eb5d0e40d61a12ccb1adcf5
Hayato Hongo <151999571+HayatoHongo@users.noreply.github.com>
2026-07-05 13:56:44 +0900

    Fix formatting and remove redundant lines in linear.py

--- linear.py
+++ linear.py
@@ -1,9 +1,6 @@
-class MyLinear:
+class Linear:
     def __init__(self, weight=0.0):
         self.weight = weight
 
     def forward(self, x):
         return self.weight * x
-
-
-MyMyLinear = MyLinear
